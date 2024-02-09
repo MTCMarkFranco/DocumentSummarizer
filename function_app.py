0ba45fcdf620301d1d3b25d0ca059fb41910634a
Mark Franco <marfra@microsoft.com>
2024-02-09 15:01:45 -0500

    handle Multiple Chunks

--- function_app.py
+++ function_app.py
@@ -3,30 +3,57 @@ import azure.functions as func
 from dotenv import load_dotenv
 import os
 import logging
+import colorlog
+import time
 from summarization_class import Summarizer
 
+class DurationFormatter(colorlog.ColoredFormatter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.start_time = time.time()
+
+    def format(self, record):
+        duration = time.time() - self.start_time
+        self.start_time = time.time()
+        record.duration = "{:.1f}".format(duration)
+        return super().format(record)
+    
+# content_chunked is an array of strings
 class Data:
-    def __init__(self, merged_content):
-        self.merged_content = merged_content
+    def __init__(self, content_chunked):
+        self.content_chunked = content_chunked
 
 class Record:
     def __init__(self, recordId, data):
         self.recordId = recordId
         self.data = data
 
+handler = colorlog.StreamHandler()
+handler.setFormatter(DurationFormatter('%(log_color)s%(levelname)s: Previous Step Time: %(duration)s(seconds). Next Step: %(message)s',
+            log_colors={
+                            'DEBUG': 'cyan',
+                            'INFO': 'green',
+                            'WARNING': 'yellow',
+                            'ERROR': 'red',
+                            'CRITICAL': 'red,bg_white',
+                        }))
+logger: logging.Logger = colorlog.getLogger("__INDEXER__")
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+    
 def summarize_document(record, summarizer):
     """Summarize a document using a given summarizer."""
     documentcontent = Data(**record.data)
-    document = documentcontent.merged_content
-    summary = summarizer.summarize(document)
-    logging.info(f"Summarizer Response: {summary}")
+    documentchunks = documentcontent.content_chunked
+    summary = summarizer.summarize(documentchunks)
+    logger.info(f"Summarizer Response: {summary}")
     return summary
 
 app = func.FunctionApp()
 
 @app.route(route="doc_summarizer",methods=["POST"], auth_level=func.AuthLevel.FUNCTION)
 def doc_summarizer(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logger.info('Python HTTP trigger function processed a request.')
     # Load environment variables from .env file
     load_dotenv()
     key = os.getenv('LANG_KEY')
@@ -38,7 +65,7 @@ def doc_summarizer(req: func.HttpRequest) -> func.HttpResponse:
         first_item = req_body.get('values', [{}])[0]
         record = Record(**first_item)
     except Exception as e:
-        logging.error(f"Error parsing request body: {e}")
+        logger.error(f"Error parsing request body: {e}")
         return func.HttpResponse(f"Error parsing request body: {e}", status_code=400)
 
     # Call summarizer
@@ -46,7 +73,7 @@ def doc_summarizer(req: func.HttpRequest) -> func.HttpResponse:
     try:
         summary = summarize_document(record, summarizer)
     except Exception as e:
-        logging.error(f"Error summarizing document: {e}")
+        logger.error(f"Error summarizing document: {e}")
         return func.HttpResponse(f"Error summarizing document: {e}", status_code=500)
 
     # Send the summary back
@@ -62,6 +89,6 @@ def doc_summarizer(req: func.HttpRequest) -> func.HttpResponse:
             }
         ]
     }
-    logging.info(f"Complete Response: {response}")
+    logger.info(f"Complete Response: {response}")
 
     return func.HttpResponse(json.dumps(response), mimetype="application/json", status_code=200)
